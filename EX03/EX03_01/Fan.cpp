9702f4e7ed988852405e9b1663058303232885ca
MrBean1512 <39568733+MrBean1512@users.noreply.github.com>
2018-06-03 17:27:37 -0700

    Delete Fan.cpp

--- EX03/EX03_01/Fan.cpp
+++
@@ -1,4 +0,0 @@
-/*
-	I made this program before our lectures/the book covered making a class.cpp file.
-The progression of each problem should show my gradual understanding as they become
-cleaner and more concise
