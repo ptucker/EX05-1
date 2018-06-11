6400f998173140946abda0dd2b2e805ede024ab3
Peter Tucker <ptucker@whitworth.edu>
2018-06-11 09:29:05 -0700

    added feedback

--- EX04_03_rewrite.cpp
+++ EX04_03_rewrite.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
+// 18/20
+
 int small(int*);
 
 int main()
@@ -10,8 +12,13 @@ int main()
 	return 0;
 }
 
+//PT -- Pass the size of the array too, so that your function can work with arrays of any size
+//      and not hard-coded to 8
+// -2
 int small(int* ary)
 {
+	//PT -- set smlst to ary[0]. Then you don't need to guess, and you might find one smaller.
+	//      also, you only go through the array once.
 	int largest=0;	//I search for the largest first so that I don't ever use an initializer that's too small
 	for (int i = 0; i < 8; i++)
 		if (ary[i] > largest)
