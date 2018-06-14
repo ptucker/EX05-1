d598a8121ffbb812b7f403eddba591e4d42d2ba0
Peter Tucker <ptucker@whitworth.edu>
2018-06-14 14:38:42 -0700

    added feedback

--- EX05_04.cpp
+++ EX05_04.cpp
@@ -7,6 +7,8 @@ using namespace std;
 template <typename T>
 void shuffle(vector<T> &v);
 
+// 17/20
+
 int main()
 {
 	int size = 10;
@@ -19,6 +21,9 @@ int main()
 	return 0;
 }
 
+//PT -- this works, but could you imagine an algorithm that matches how cards are actually sorted
+//      that is, cut the deck somewhat in half, then merge the two "halves"?
+
 template <typename T>
 void shuffle(vector<T> &v)
 {
@@ -26,9 +31,17 @@ void shuffle(vector<T> &v)
 	if (n > 1)
 	{
 		srand(time(NULL));
+		//PT -- should be int i, since an index in an array is always an int.
+		// -1 
 		for (T i = 0; i < n - 1; i++)
 		{
+			//PT -- should be int j
+			// It's not clear to me what j is in this case. A comment would help.
+			// I would expect int j = rand() % n;
+			//  -1
 			T j = i + rand() / (RAND_MAX / (n - i) + 1);
+			//PT -- should be T t = v[j], since you're getting a T-type thing out of v.
+			// -1
 			int t = v[j];
 			v[j] = v[i];
 			v[i] = t;
